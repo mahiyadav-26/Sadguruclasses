f5a007f3ffffafdefbc858acda2497365683ff2c
Mahima Academy <mahimaacademe@gmail.com>
2026-09-12 21:42:07 +0530

    feat: render FloatingAuthButton on landing page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,6 +14,7 @@ import LeadForm from "../components/Landing/LeadForm";
 import Testimonials from "../components/Landing/Testimonials";
 import Footer from "../components/Landing/Footer";
 import WhatsAppButton from "../components/common/WhatsAppButton";
+import FloatingAuthButton from "../components/common/FloatingAuthButton";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function Index() {
@@ -66,6 +67,7 @@ export default function Index() {
       </main>
       <Footer ref={footerRef} />
       {!user && <WhatsAppButton />}
+      <FloatingAuthButton />
     </div>
   );
 }
